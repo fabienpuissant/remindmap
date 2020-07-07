a06301e122584e8c2379d25d352d4950c54ad92e
Fabien Puissant <fabienpuissant@live.fr>
2020-07-08 01:16:27 +0200

    init

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      location: null
+    }
+  }
+
+  componentWillMount() {
+    Geolocation.getCurrentPosition(info => console.log(info));
+  }
+
+
+  render() {
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+      },
+    });
+    return(
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <MapView
+    style={styles.mapStyle}
+    initialRegion={{
+      latitude: this.state.location.latitude,
+      longitude: this.state.location.longitude,
+    }}
+  />
+  </View>
+  </>
+  )
+  }
+
+ 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
