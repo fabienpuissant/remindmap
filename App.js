2c3a396f42249e5f7a0fdb696f6a93632537b813
Fabien Puissant <fabienpuissant@live.fr>
2020-07-09 09:17:11 +0200

    changes

--- App.js
+++ App.js
@@ -1,51 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
-import Geolocation from '@react-native-community/geolocation';
 
-export default class App extends React.Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      location: null
-    }
+
+export default function App() {
+
+  const [latitude, setLatitude] = useState(0)
+
+  const [longitude, setLongitude] = useState(0)
+
+  const [accuracy, setAccuracy] = useState(0)
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    mapStyle: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  });
+
+
+  const initPosition = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+        setAccuracy(position.coords.accuracy)
+      }
+    )
+  }
+  initPosition()
+
+  const regionFrom = () => {
+    const { width, height } = Dimensions.get('window');
+
+    const ASPECT_RATIO = width / height;
+    const latDelta = 0.05;
+    const lngDelta = latDelta * ASPECT_RATIO;
+
+    return {
+      latitude: latitude,
+      longitude: longitude,
+      latitudeDelta: latDelta,
+      longitudeDelta: lngDelta
+    };
   }
 
-  componentWillMount() {
-    Geolocation.getCurrentPosition(info => console.log(info));
+  const region = regionFrom();
+
+  console.log(region)
+/*
+  const region = {
+    "latitude": 40.4152855,
+    "latitudeDelta": 0.0922,
+    "longitude": -3.7006722,
+    "longitudeDelta": 0.04815087040618956,
   }
-
-
-  render() {
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      mapStyle: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-      },
-    });
-    return(
+*/
+  return(
     <>
     <View style={styles.container}>
     <MapView
     style={styles.mapStyle}
-    initialRegion={{
-      latitude: this.state.location.latitude,
-      longitude: this.state.location.longitude,
-    }}
+    initialRegion={region}
   />
   </View>
   </>
   )
-  }
-
- 
 }
+
 
